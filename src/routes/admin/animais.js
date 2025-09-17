fa43cf942aaec637e15238e1b6cf1302600cd50e
luanagreys7 <luanagreys7@gmail.com>
2025-09-17 11:54:15 -0300

    Fix nome função GetAnimalPorID

--- src/routes/admin/animais.js
+++ src/routes/admin/animais.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { listarAnimais, atualizarAnimal, deletarAnimal, getAnimalByID } from '../../controllers/adminAnimaisController.js';
+import { listarAnimais, atualizarAnimal, deletarAnimal, getAnimalPorID } from '../../controllers/adminAnimaisController.js';
 import authAdmin from '../../middlewares/authAdmin.js';
 
 const routerAdm = express.Router();
 
-routerAdm.get('/animais/:id', authAdmin, getAnimalByID);
+routerAdm.get('/animais/:id', authAdmin, getAnimalPorID);
 routerAdm.get('/admin/animais', authAdmin, listarAnimais);
 routerAdm.patch('/admin/animais/:id', authAdmin, atualizarAnimal);
 routerAdm.delete('/admin/animais/:id', authAdmin, deletarAnimal);
