9b400814e32cd7228ab6420b5c3ff734d6bc0836
juliaaamendes <juliaaa.mendes.p@gmail.com>
2025-10-01 10:38:03 -0300

    coreção adoção

--- src/routes/routers.js
+++ src/routes/routers.js
@@ -4,6 +4,7 @@ import authRoutes from '../routes/tutor/authLogin.js';
 import routerAnimal from '../routes/animal/animal.js';
 import routerAdocao from '../routes/adocao/adocaoRoutes.js';
 import routerQuestionario from '../routes/tutor/questionario.js';
+import routerDoacao from '../routes/doacao/doacao.js';
 
 const router = express.Router();
 
@@ -20,7 +21,7 @@ const router = express.Router();
     router.use(routerAdocao);
 
     // Rota doação
-    
+
     router.use(routerDoacao);
 
     //Rota questionario
