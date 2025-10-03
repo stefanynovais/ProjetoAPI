d13988eda5e50317917e00b5f8c47c79f91a4ee0
stefanynovais <stefany.nascimento202etec.sp.gov.br>
2025-10-03 17:26:43 -0300

    Comitando

--- src/seeders/usuarioSeeder.js
+++ src/seeders/usuarioSeeder.js
@@ -1,14 +1,13 @@
 import { connectDatabase } from '../database/database.js';
-import UsuarioModel from '../models/Usuario.js';
+import Usuario from '../models/Usuario.js';
 import bcrypt from 'bcrypt'; //biblioteca que criptrografa a senha
 
 const seed = async () => {
   try {
-    //conectando ao banco e pegando a instância do Sequelize
-    const sequelize = await connectDatabase();
+    //conectando ao banco 
+    await connectDatabase();
 
-    //importando o model passando o Sequelize
-    const Usuario = UsuarioModel(sequelize);
+  
 
     //sincroniza a tabela (force: true apaga dados antigos)
     await Usuario.sync({ force: true });
@@ -16,7 +15,6 @@ const seed = async () => {
     //criando usuário de teste com senha hash (frheiu248hf93)
     //essa função transforma a senha original em uma sequência de caracteres única, que não pode ser facilmente revertida para descobrir a senha original.
     //usamos essa senha "hash" por segurança, se o banco for invadido, o hacker não vai conseguir ver a senha real
-    const senhaHash = await bcrypt.hash('123456', 10); //"senhaHash" senha hash resultante; senha original, 10 salt rounds (quantas vezes o algoritmo vai embaralhar a senha)
     await Usuario.create({
       nome_completo: "Teste Usuario",
       email: "teste@teste.com",
